bce3e20bac51db55073e532a359c006bd305617e
Igor Kamyshev <garik.novel@gmail.com>
2018-03-28 10:26:11 +0700

    Some fixes in structure

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -6,11 +6,17 @@ class SearchBox extends React.Component {
 
     render () {
         return (
-            <input
-                className={`search-box ${this.state.fixed ? 'search-box_fixed' : null}`}
-                placeholder="Поиск"
-            >
-            </input>
+            <section className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <input
+                            className={`search-box ${this.state.fixed ? 'search-box_fixed' : null}`}
+                            placeholder="Поиск"
+                        >
+                        </input>
+                    </div>
+                </div>
+            </section>
         );
     }
 
