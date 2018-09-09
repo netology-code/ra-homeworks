c64cffd8ba5b3f4352cbe0203f4176f7a31d0f34
Nickolay Svetlov <ns@idaproject.com>
2018-09-09 14:12:23 +0300

    add currentMonthDays, prevMonthDays, nextMonthDays

--- jsx/calendar/js/Calendar.js
+++ jsx/calendar/js/Calendar.js
@@ -1,11 +1,14 @@
 const Calendar = (props) => {
   const {date} = props;
-  const prevMonth = getPreviousMonth(date);
-  const nextMonth = getNextMonth(date);
   const dayString = capitalizeString(date.toLocaleString('ru-ru', {weekday: 'long'}));
   const decliningMonth = date.toLocaleString('ru', {month: 'long', day: 'numeric'}).split(' ')[1];
-  const year = date.getFullYear();
   const month = capitalizeString(date.toLocaleString('ru', {month: 'long'}));
+  const year = date.getFullYear();
+  const prevMonth = getPreviousMonth(date);
+  const nextMonth = getNextMonth(date);
+  const prevMonthDays = getPreviousDays(date.getDay(), prevMonth);
+  const currentMonthDays = getCurrentDays(date);
+  const nextMonthDays = getNextDays(date.getDay(), nextMonth);
 
   return (
     <div className="ui-datepicker">
@@ -44,24 +47,24 @@ const Calendar = (props) => {
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td className="ui-datepicker-other-month">27</td>
-          <td className="ui-datepicker-other-month">28</td>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td>4</td>
-          <td>5</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>7</td>
-          <td className="ui-datepicker-today">8</td>
-          <td>9</td>
-          <td>10</td>
-          <td>11</td>
-          <td>12</td>
-        </tr>
+        {/*<tr>*/}
+        {/*<td className="ui-datepicker-other-month">27</td>*/}
+        {/*<td className="ui-datepicker-other-month">28</td>*/}
+        {/*<td>1</td>*/}
+        {/*<td>2</td>*/}
+        {/*<td>3</td>*/}
+        {/*<td>4</td>*/}
+        {/*<td>5</td>*/}
+        {/*</tr>*/}
+        {/*<tr>*/}
+        {/*<td>6</td>*/}
+        {/*<td>7</td>*/}
+        {/*<td className="ui-datepicker-today">8</td>*/}
+        {/*<td>9</td>*/}
+        {/*<td>10</td>*/}
+        {/*<td>11</td>*/}
+        {/*<td>12</td>*/}
+        {/*</tr>*/}
         </tbody>
       </table>
     </div>
@@ -69,16 +72,63 @@ const Calendar = (props) => {
 };
 
 function getPreviousMonth(currentDate) {
-  const newDate = new Date(currentDate.getTime());
+  /*new Date принимает currentDate.getFullYear() и currentDate.getMonth(), а не currentDate.getTime().
+  Причина данной реализациии состоит в том, что если текущая дата 31.12.2018, то newDate.getMonth() вернет не предыдущий
+  месяц, а 1.12.2018
+  */
+  const newDate = new Date(currentDate.getFullYear(), currentDate.getMonth());
+
   newDate.setMonth(newDate.getMonth() - 1);
   return newDate;
 
 }
 
 function getNextMonth(currentDate) {
-  const newDate = new Date(currentDate.getTime());
+  const newDate = new Date(currentDate.getFullYear(), currentDate.getMonth());
+
   newDate.setMonth(newDate.getMonth() + 1);
   return newDate;
+}
+
+function getPreviousDays(weekDay, month) {
+  const counter = weekDay === 0 ? 6 : weekDay - 1;
+  const lastDayOfMonth = new Date(month.getFullYear(), month.getMonth() + 1, 0);
+  const day = lastDayOfMonth.getDate();
+  const result = collectPrevDays(counter, day);
+
+  return result.reverse();
+}
+
+function getCurrentDays(date) {
+  const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const lastDay = lastDayOfMonth.getDate();
+
+  return collectNextDays(lastDay);
+}
+
+function getNextDays(weekDay, month) {
+  const counter = weekDay === 0 ? weekDay : 7 - weekDay;
+
+  const result = collectNextDays(counter);
+  return result;
+}
+
+function collectPrevDays(i, prevDay, list = []) {
+  if (i === 0) return list;
+
+  const day = list.length < 1 ? prevDay : prevDay - 1;
+  list.push(day);
+
+  return collectPrevDays(i - 1, day, list);
+}
+
+function collectNextDays(i, prevDay = 1, list = []) {
+  if (i === 0) return list;
+
+  const day = list.length < 1 ? prevDay : prevDay + 1;
+  list.push(day);
+
+  return collectNextDays(i - 1, day, list);
 }
 
 function capitalizeString(string) {
